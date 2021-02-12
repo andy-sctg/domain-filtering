89abeef2d26bbd91f23d6eae9c10fe779a969543
astocchetti <astocchetti@servercentral.com>
2021-02-11 18:20:56 -0600

    check url status

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import requests
+
 bad_tld = [
   '.ru',
   '.poop',
@@ -21,11 +23,30 @@ def cleanDomain(domain):
   domain = domain.strip('/')
   return domain
 
+def getStatus(domain, prefix='https://'):
+  url = prefix + domain
+  try:
+    r = requests.get(url)
+    return r.status_code
+  except:
+    return -1
+
+def checkDomain(domain):
+  secureStatus = getStatus(domain)
+  # insecureStatus = getStatus(domain, prefix='http://')
+  return [domain, secureStatus]
+
+
 my_file = open('domains.txt', 'r')
 all_domains = my_file.readlines()
 all_domains = map(cleanDomain, all_domains)
 
 target_domains = filter(isWorthyDomain, all_domains)
-with open('sales_goals.txt', 'w') as f:
-    for domain in target_domains:
-        f.write("%s\n" % domain)
+domain_status = map(checkDomain, target_domains)
+
+with open('sales_goals.csv', 'w') as f:
+  f.write('web domain,website response code\n')
+  for data in domain_status:
+    line = data[0] + ',' + str(data[1]) + '\n'
+    print(line)
+    f.write(line)
